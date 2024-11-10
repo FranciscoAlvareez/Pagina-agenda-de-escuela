304bdd1fc7788a442f2e57a7874f2939a0db8d6d
IgnacioMartinez01 <igmartinezpintosgmail.com>
2024-11-10 05:28:35 -0300

    login register style

--- agenda_escuela/src/Components/Forms/index.jsx
+++ agenda_escuela/src/Components/Forms/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import classes from "./index.module.css"
-
+import logoProyecto from "../../assets/Icons/logoProyecto.png";
 function Forms({ title }) {
   // Estados para almacenar los valores de los inputs
   const [name, setName] = useState("");
@@ -21,77 +21,76 @@ function Forms({ title }) {
     console.log("Formulario enviado:", { name, lastname, ci, contactnumber, password, mail, dateBirth });
     navigate("/home");
   };
-
   return (
     // Formulario de Login
-    <>
-    <img src="agenda_escuela\src\logoProyecto.png" alt="logo" />
-    <div className={classes.Login}>
-      <h2>{title}</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Mail"
-          className={classes.input}
-          value={mail}
-          onChange={(e) => setMail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          className={classes.input}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {title === "Register" && (
-          <>
-            <input
-              type="text"
-              placeholder="Nombre"
-              className={classes.input}
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <input
-              type="text"
-              placeholder="Apellido"
-              className={classes.input}
-              value={lastname}
-              onChange={(e) => setLastname(e.target.value)}
-            />
-            <input
-              type="number"
-              placeholder="Numero de contacto"
-              className={classes.input}
-              value={contactnumber}
-              onChange={(e) => setContactnumber(e.target.value)}
-            />
-            <input
-              type="date"
-              placeholder="Fecha de Nacimiento"
-              className={classes.input}
-              value={dateBirth}
-              onChange={(e) => setDateBirth(e.target.value)}
-            />
-          </>
-        )}
-        <button type="submit">{title}</button>
-      </form>
-      <div className="link-container">
-        {title === "Login" ? (
-          <p>
-            ¿Aún no estás registrado?{" "}
-            <Link to="/register">Regístrate</Link>
-          </p>
-        ) : (
-          <p>
-            ¿Ya tienes una cuenta?{" "}
-            <Link to="/login">Inicia sesión</Link>
-          </p>
-        )}
+    <div className="container">
+      <div className="image">
+        <img src={logoProyecto} alt="logo" />
+      </div>
+      <div className={classes.Login}>
+        <h2>{title}</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Mail"
+            className={classes.input}
+            value={mail}
+            onChange={(e) => setMail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            className={classes.input}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {title === "Register" && (
+            <>
+              <input
+                type="text"
+                placeholder="Nombre"
+                className={classes.input}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Apellido"
+                className={classes.input}
+                value={lastname}
+                onChange={(e) => setLastname(e.target.value)}
+              />
+              <input
+                type="number"
+                placeholder="Numero de contacto"
+                className={classes.input}
+                value={contactnumber}
+                onChange={(e) => setContactnumber(e.target.value)}
+              />
+              <input
+                type="date"
+                placeholder="Fecha de Nacimiento"
+                className={classes.input}
+                value={dateBirth}
+                onChange={(e) => setDateBirth(e.target.value)}
+              />
+            </>
+          )}
+          <button type="submit">{title}</button>
+        </form>
+        <div className="link-container">
+          {title === "Login" ? (
+            <p>
+              ¿Aún no estás registrado? <Link to="/register">Regístrate</Link>
+            </p>
+          ) : (
+            <p>
+              ¿Ya tienes una cuenta? <Link to="/login">Inicia sesión</Link>
+            </p>
+          )}
+        </div>
       </div>
     </div>
-    </>
   );
 }
 
